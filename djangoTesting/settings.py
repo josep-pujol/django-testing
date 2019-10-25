108e7b0483740d64efee7ff66035dbbf4adebb9c
Ubuntu <ubuntu@ip-172-31-81-6.ec2.internal>
2019-10-25 20:50:11 +0000

    configuration for  static files

--- djangoTesting/settings.py
+++ djangoTesting/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [   
     os.getenv('C9_HOSTNAME'),  # Cloud9
-    os.getenv('HOSTNAME'),   # Heroku
-    os.getenv('DATABASE_URL'), # Postgres in AWS
+    os.getenv('HOSTNAME'),     # Heroku
+    os.getenv('DATABASE_URL'), # Postgres
 ] 
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,5 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
