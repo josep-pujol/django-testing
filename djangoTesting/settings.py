2ce0afe74ac61f297bfe4f768b56fb420f854329
Ubuntu <ubuntu@ip-172-31-81-6.ec2.internal>
2019-10-26 22:25:31 +0000

    added dj_database_url to db configs

--- djangoTesting/settings.py
+++ djangoTesting/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,9 +91,7 @@ if os.getenv('DEVELOPMENT'):
     }
 else:
     DATABASES = {
-        'default': {
-            'HOST': os.getenv('DATABASE_URL')
-        }
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     }
 
 # Password validation
